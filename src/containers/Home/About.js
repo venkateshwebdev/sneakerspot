84bdbd4a7381a34c5f8c98b40cd96f662b1b5593
venkateshwebdev <nikhilsirigineedi3@gmail.com>
2023-06-22 20:48:14 +0530

    inital commit

--- src/containers/Home/About.js
+++ src/containers/Home/About.js
@@ -18,11 +18,12 @@ const About = () => {
     useEffect(()=>{
         Aos.init({duration:2000,easing:"ease-in-out"})
     },[])
+    console.log("I am rendered")
     return ( 
         <div className="about-container">
                         <div>
                 <div className="about-head">Top Brands For You</div>
-                <div className="about-trending" data-aos="zoom-in">
+                <div className="about-trending" >
                     <img src={"https://images-cs.stockx.com/v3/assets/blt818b0c67cf450811/blt7486bb7a2357dbbe/646ceae3a15c658a033d29d3/nikemay.jpg?quality=80&auto=webp&format=pjpg&dpr=1&width=234&height=160"}/>
                     <img src={"https://images-cs.stockx.com/v3/assets/blt818b0c67cf450811/blt75bf3b15c3aae08c/646ceaf3c9c6e238e3299f15/adidasmay.jpg?quality=80&auto=webp&format=pjpg&dpr=1&width=234&height=160"}/>
                     <img src={"https://images-cs.stockx.com/v3/assets/blt818b0c67cf450811/blt4fa8028e057af36f/646ceb0112041606a87c765c/newbalancemay.jpg?quality=80&auto=webp&format=pjpg&dpr=1&width=234&height=160"}/>
@@ -31,7 +32,8 @@ const About = () => {
             </div>
 
             <div>
-                <div className="about-trending about-gal" data-aos="zoom-in">
+            <div className="about-head">Offers</div>
+                <div className="about-trending about-gal" >
                     
                     <img src={"https://images-cs.stockx.com/v3/assets/blt818b0c67cf450811/blt227316ff2598f358/6491f77a352ac626a8da498a/Summer_Editorial-_Get_up_&_go_slips_onsSecondaryA.jpg?format=jpg&auto=webp&height=438"} />
                     <img src={"https://images.unsplash.com/photo-1626379616459-b2ce1d9decbc?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fHNuZWFrZXJzfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60"} height={"438px"} />
@@ -42,17 +44,17 @@ const About = () => {
             </div>
             </div>
             <div>
-                <div className="about-head">Trending</div>
-                <div className="about-trending" data-aos="zoom-in">
+                <div className="about-head ">Trending</div>
+                <div className="about-trending about-prod" >
                 {
-                    data?.data.trend.map((e,i)=><ProductCard key={i} home={true} image={e.images?.[1]?e.images[1]:e.images?.[0]}/>)
+                    data?.data.trend.map((e,i)=><ProductCard key={i} home={true} price={e.price} image={e.images?.[1]?e.images[1]:e.images?.[0]}/>)
                 }
             </div>
             </div>
                         <div>
                 <div className="about-head">Tag us on Social Media</div>
                 <p>Using #SneakerSpot to get featured our website</p>
-                <div className="about-trending about-gal" data-aos="zoom-in">
+                <div className="about-trending about-gal" >
                 <img src={"https://images.unsplash.com/photo-1594250945177-00a894c9fa06?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=465&q=80"} height={"400px"}/>
                     <img src={"https://plus.unsplash.com/premium_photo-1665675242030-54da84ce57c5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80"} height={"400px"}/>
                     <img src={"https://images.unsplash.com/photo-1634080271817-dcd33806ac66?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80"} height={"400px"}/>
@@ -65,7 +67,7 @@ const About = () => {
             </div>
             {/* <div>
                 <div className="about-head">adidas</div>
-                <div className="about-trending" data-aos="zoom-in">
+                <div className="about-trending" >
                 {
                     data?.data.adidas.map((e,i)=><ProductCard key={i} home={true} image={e.images?.[1]?e.images[1]:e.images?.[0]} title={e.title} price={e.price}/>)
                 }
@@ -73,7 +75,7 @@ const About = () => {
             </div> */}
             {/* <div>
                 <div className="about-head">Puma</div>
-                <div className="about-trending" data-aos="zoom-in">
+                <div className="about-trending" >
                 {
                     data?.data.puma.map((e,i)=><ProductCard key={i} home={true} image={e.images?.[1]?e.images[1]:e.images?.[0]} title={e.title} price={e.price}/>)
                 }
